413512ef2bc0cd3c07d205dcae65f64f99f64a1d
Mina Messiha <mena.sb.109@gmail.com>
2021-02-22 11:29:40 -0800

    Added message when no args are provided

--- robinhood_portfolio_manager/__init__.py
+++ robinhood_portfolio_manager/__init__.py
@@ -1,13 +1,28 @@
+########################################################
+# A python CLI that allows a Robinhood user to easily
+# allocate and divide his capital on different stocks.
+########################################################
+# Author: Mina Messiha
+# Copyright: Copyright 2021, robinhood-portfolio-manager
+# License: MIT License
+# Version: 0.0a7
+# Email: mena.sb.109@gmail.com
+# URLs: pypi.org/project/robinhood-portfolio-manager &
+# github.com/MinaMessiha109/robinhood_portfolio_manager
+# Status: In development
+########################################################
+
+
 import argparse
 from . import Robinhood
 
+
 def main():
 	kwargs = {}
+	
 	parser = argparse.ArgumentParser()
-
 	parser.add_argument("-c", "--credentials", action="store", nargs=3, required=False, help="Username, password, and TOTP separated by a space", dest="creds")
 	parser.add_argument("-s", "--simulation", action="store_true", required=False, help="Run simulation without placing actual orders", dest="sim")
-
 	parser.add_argument("-g", "--generate_file", action="store", required=False, choices=["csv", "excel"], help="Generate excel/csv file", dest="generate_file")
 	parser.add_argument("-n", "--get_new_investments", action="store_true", required=False, help="Get new investments", dest="get_new_investments")
 	parser.add_argument("-o", "--get_current_investments", action="store_true", required=False, help="Get current investments", dest="get_current_investments")
@@ -15,9 +30,10 @@ def main():
 	parser.add_argument("-r", "--rebalance", action="store_true", required=False, help="Rebalance portfolio", dest="rebalance")
 	parser.add_argument("-d", "--debug", action="store_true", required=False, help="Run test function for debugging", dest="debug")
 	parser.add_argument("-a", "--cancel_open_orders", action="store", choices=["all", "sell", "buy"], required=False, help="Cancel all open orders", dest="cancel_open_orders")
-
-
 	args = parser.parse_args()
+
+	if not any(vars(args).values()):
+		parser.error("No arguments provided. Enter a valid argument or '--help' to see available arguments.")
 
 	if args.creds:
 		kwargs["username"] = args.creds[0]
