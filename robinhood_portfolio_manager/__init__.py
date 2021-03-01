67aee9004f198b21dc68d6059b9c1116e1d8595a
Mina Messiha <mena.sb.109@gmail.com>
2021-02-28 17:49:59 -0800

    Automatic Update

--- robinhood_portfolio_manager/__init__.py
+++ robinhood_portfolio_manager/__init__.py
@@ -4,7 +4,7 @@
 # Author: Mina Messiha
 # Copyright: Copyright 2021, robinhood-portfolio-manager
 # License: MIT License
-# Version: 0.0b3
+# Version: 0.0b6
 # Date: 02-25-2021
 # Email: mena.sb.109@gmail.com
 # URLs: pypi.org/project/robinhood-portfolio-manager &
